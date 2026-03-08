8a09176e7bf5f602b74bf9d533711c8acf296b86
birnb <birnb@THINKPADP16>
2026-03-08 17:21:10 +0100

    fis Dashboard

--- Dashboard/SettingsModels.cs
+++ Dashboard/SettingsModels.cs
@@ -2,15 +2,15 @@ namespace Dashboard
 {
     public class BrokerSettings
     {
-        public string Host { get; set; } = "localhost";
-        public int Port { get; set; } = 1883;
-        public string Username { get; set; } = "EWON";
-        public string Password { get; set; } = "admin123";
-        public bool IsLocal { get; set; } = true;
+        public string Host { get; set; } = "e21df7393cc24e69b198158d3af2b3d6.s1.eu.hivemq.cloud";
+        public int Port { get; set; } = 8883;
+        public string Username { get; set; } = "USCotton";
+        public string Password { get; set; } = "Admin123";
+        public bool IsLocal { get; set; } = false;
 
         public string GetSubscribeTopic()
         {
-            return IsLocal ? "local/#" : "cloud/*";
+            return IsLocal ? "local/#" : "cloud/#";
         }
 
         public string GetPublishTopicPrefix()
